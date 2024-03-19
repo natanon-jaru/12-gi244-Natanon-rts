e9a194d421a54e68808c92d442fcc22da7ab3b60
natanon <natanon.jari@bumail.net>
2024-03-19 16:10:33 +0700

    add 3 Empty Object(Units,Buildings,GhostBuildings) at Britain and Pirate

--- Assets/Scripts/Buildings/Building.cs
+++ Assets/Scripts/Buildings/Building.cs
@@ -41,7 +41,7 @@ public class Building : Structure
     // Start is called before the first frame update
     void Start()
     {
-        
+                
     }
 
     // Update is called once per frame
