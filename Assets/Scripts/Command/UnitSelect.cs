6bae0aa1bf463a489e3cfc1dba90b68c4a0aa9c9
natanon <natanon.jari@bumail.net>
2024-04-04 10:36:29 +0700

    damaged Hp real-time

--- Assets/Scripts/Command/UnitSelect.cs
+++ Assets/Scripts/Command/UnitSelect.cs
@@ -29,6 +29,9 @@ public class UnitSelect : MonoBehaviour
     private Vector2 oldAnchoredPos;//Box old anchored position
     private Vector2 startPos;//point where mouse is down
 
+    private float timer = 0f;
+    private float timeLimit = 0.5f;
+
     [SerializeField]
     private Unit curEnemy;
 
@@ -76,7 +79,15 @@ public class UnitSelect : MonoBehaviour
             ReleaseSelectionBox(Input.mousePosition);
             TrySelect(Input.mousePosition);
         }
-        
+
+        timer += Time.deltaTime;
+
+        if (timer >= timeLimit)
+        {
+            timer = 0f;
+            UpdateUI();
+        }
+
 
     }
     
@@ -141,6 +152,8 @@ public class UnitSelect : MonoBehaviour
         ClearAllSelectionVisual();
         CurUnits.Clear();
         curBuilding = null;
+        curResource = null;
+        curEnemy = null;
         
         //ClearUI
         InfoManager.instance.ClearAllInfo();
@@ -250,6 +263,22 @@ public class UnitSelect : MonoBehaviour
         InfoManager.instance.ShowEnemyAllInfo(b);
     }
 
+    private void UpdateUI()
+    {
+        if (curUnits.Count == 1)
+            ShowUnit(curUnits[0]);
+        else if (curEnemy != null)
+            ShowEnemyUnit(curEnemy);
+        else if (curResource != null)
+            ShowResource();
+        else if (curBuilding != null)
+        {
+            if (GameManager.instance.MyFaction.IsMyBuilding(curBuilding))
+                ShowBuilding(curBuilding);//Show building info
+            else
+                ShowEnemyBuilding(curBuilding);
+        }
+    }
 
     
 }
