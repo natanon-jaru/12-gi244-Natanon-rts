ffe0b4b26c202a7bbb76f9b241f69852bd350926
natanon <natanon.jari@bumail.net>
2024-04-13 11:12:39 +0700

    UI

--- Assets/Scripts/Command/UnitSelect.cs
+++ Assets/Scripts/Command/UnitSelect.cs
@@ -77,6 +77,10 @@ public class UnitSelect : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             ReleaseSelectionBox(Input.mousePosition);
+
+            if (IsPointerOverUIObject())
+                return;
+            
             TrySelect(Input.mousePosition);
         }
 
@@ -278,6 +282,16 @@ public class UnitSelect : MonoBehaviour
             else
                 ShowEnemyBuilding(curBuilding);
         }
+    }
+    
+    //When Touching UI
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
     }
 
     
